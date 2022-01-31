6ada19642a7a6144bbb179eb5e5cc2ec56c888bf
cazmaar <kazeem.alatishe@yahoo.com>
2022-01-31 00:08:11 +0000

    useAuth in App/index.js deleted

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,7 @@ import css from "./App.module.css";
 import Display from "../Display";
 import { Routes, Route } from "react-router-dom";
 import Welcome from "../Welcome";
-import { useAuth0 } from "@auth0/auth0-react";
+
 
 function App() {
   return (
